9c4f7855df5851dcc89860996e095cb802eafaa4
Robert <robertreems@gmail.com>
2025-03-04 19:54:17 +0100

    testing

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 from app import app
 # , start_background_tasks
 
-# Start achtergrondtaken hier
-# start_background_tasks()
+# Deze moet BUITEN `if __name__ == "__main__"` staan
+start_background_tasks()  # Zorgt dat de achtergrondthread start, ook met Gunicorn!
 
+# Alleen `app.run()` binnen `if __name__ == "__main__"`
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, use_reloader=False)
